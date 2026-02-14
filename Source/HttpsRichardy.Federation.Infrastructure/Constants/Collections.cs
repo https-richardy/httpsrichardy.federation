889bdb92771708261d99c88f1599b4aa9ae05109
Richard Garcia <code.richardy@gmail.com>
2026-02-14 12:05:37 -0300

    chore: the names of the collections have been updated to remove the duplicate prefix “federation.federation.”, now using only “federation.” for all collections.

--- Source/HttpsRichardy.Federation.Infrastructure/Constants/Collections.cs
+++ Source/HttpsRichardy.Federation.Infrastructure/Constants/Collections.cs
@@ -2,11 +2,11 @@ namespace HttpsRichardy.Federation.Infrastructure.Constants;
 
 public static class Collections
 {
-    public const string Users = "federation.federation.users";
-    public const string Permissions = "federation.federation.permissions";
-    public const string Groups = "federation.federation.groups";
-    public const string Tokens = "federation.federation.tokens";
-    public const string Realms = "federation.federation.realms";
-    public const string Scopes = "federation.federation.scopes";
-    public const string Secrets = "federation.federation.secrets";
+    public const string Users = "federation.users";
+    public const string Permissions = "federation.permissions";
+    public const string Groups = "federation.groups";
+    public const string Tokens = "federation.tokens";
+    public const string Realms = "federation.realms";
+    public const string Scopes = "federation.scopes";
+    public const string Secrets = "federation.secrets";
 }
