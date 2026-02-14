cc77bc27cfd3cfcb463f0e2bea7f783ac505fb08
Richard Garcia <code.richardy@gmail.com>
2026-02-14 11:42:27 -0300

    chore: the title displayed in the documentation has been changed to “Federation | Reference” in the specifications extension, making it more generic.

--- Source/HttpsRichardy.Federation.WebApi/Extensions/SpecificationExtension.cs
+++ Source/HttpsRichardy.Federation.WebApi/Extensions/SpecificationExtension.cs
@@ -14,7 +14,7 @@ public static class SpecificationsExtension
             options.HideSearch = true;
             options.Servers = [];
 
-            options.WithTitle("ichardy Federation | Reference");
+            options.WithTitle("Federation | Reference");
             options.WithClassicLayout();
             options.ExpandAllTags();
             options.AddPreferredSecuritySchemes(SecuritySchemes.Bearer);
