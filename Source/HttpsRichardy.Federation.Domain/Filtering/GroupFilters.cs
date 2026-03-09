99a594a3987a01ed237483a01d2e9641e2eb1f82
Richard Garcia <code.richardy@gmail.com>
2026-03-09 16:14:29 -0300

    fix(#1): this commit adds filtering by multiple identifiers

--- Source/HttpsRichardy.Federation.Domain/Filtering/GroupFilters.cs
+++ Source/HttpsRichardy.Federation.Domain/Filtering/GroupFilters.cs
@@ -4,6 +4,7 @@ public sealed class GroupFilters : Filters
 {
     public string? RealmId { get; set; }
     public string? Name { get; set; }
+    public string[]? Identifiers { get; set; }
 
     public static GroupFiltersBuilder WithSpecifications() => new();
 }
