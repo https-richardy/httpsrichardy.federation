637ade16e53400a2c4583b60c0fc7e2a53658e00
Richard Garcia <code.richardy@gmail.com>
2026-03-09 16:14:58 -0300

    fix(#1): this commit adds filtering by multiple identifiers to the mongo database filter construction pipeline

--- Source/HttpsRichardy.Federation.Infrastructure/Pipelines/GroupFiltersStage.cs
+++ Source/HttpsRichardy.Federation.Infrastructure/Pipelines/GroupFiltersStage.cs
@@ -9,6 +9,7 @@ public static class GroupFiltersStage
         var definitions = new List<FilterDefinition<BsonDocument>>
         {
             FilterDefinitions.MatchIfNotEmpty(Documents.Group.Id, filters.Id),
+            FilterDefinitions.MustBeInIfNotEmpty(Documents.Group.Id, filters.Identifiers),
             FilterDefinitions.MatchIfNotEmpty(Documents.Group.Name, filters.Name),
             FilterDefinitions.MatchIfNotEmpty(Documents.Group.RealmId, realm?.Id),
             FilterDefinitions.MatchBool(Documents.Group.IsDeleted, filters.IsDeleted),
