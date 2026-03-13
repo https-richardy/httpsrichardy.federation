2b7749b35f42883df1ba757cbe91c98c6689313b
Richard Garcia <code.richardy@gmail.com>
2026-03-13 01:14:30 -0300

    feature(#6): this commit introduces security policy restrictions/checks so that reserved permissions cannot be created, preventing privilege escalation.

--- Source/HttpsRichardy.Federation.Application/Handlers/Permission/PermissionCreationHandler.cs
+++ Source/HttpsRichardy.Federation.Application/Handlers/Permission/PermissionCreationHandler.cs
@@ -1,11 +1,18 @@
 namespace HttpsRichardy.Federation.Application.Handlers.Permission;
 
-public sealed class PermissionCreationHandler(IPermissionCollection collection, IRealmProvider realmProvider) :
+public sealed class PermissionCreationHandler(IPermissionCollection collection, IPermissionNamespacePolicy policy, IRealmProvider realmProvider) :
     IDispatchHandler<PermissionCreationScheme, Result<PermissionDetailsScheme>>
 {
     public async Task<Result<PermissionDetailsScheme>> HandleAsync(PermissionCreationScheme parameters, CancellationToken cancellation = default)
     {
         var realm = realmProvider.GetCurrentRealm();
+        var result = await policy.EnsurePermissionIsAllowedAsync(realm, new() { Name = parameters.Name }, cancellation);
+
+        if (result.IsFailure)
+        {
+            return Result<PermissionDetailsScheme>.Failure(PermissionErrors.PermissionNameIsReserved);
+        }
+
         var filters = PermissionFilters.WithSpecifications()
             .WithName(parameters.Name)
             .Build();
