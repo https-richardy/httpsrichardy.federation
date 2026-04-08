5f3836a0936250d9217265b88681b49367b2ba38
Richard Garcia <code.richardy@gmail.com>
2026-04-08 08:51:06 -0300

    feature(#20): this commit introduces client support in bootstrapper extension and update client credentials handling

--- Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Extensions/BootstrapperExtension.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Extensions/BootstrapperExtension.cs
@@ -8,6 +8,7 @@ public static class BootstrapperExtension
         using var scope = builder.ApplicationServices.CreateScope();
 
         var realmCollection = scope.ServiceProvider.GetRequiredService<IRealmCollection>();
+        var clientCollection = scope.ServiceProvider.GetRequiredService<IClientCollection>();
         var userCollection = scope.ServiceProvider.GetRequiredService<IUserCollection>();
         var permissionCollection = scope.ServiceProvider.GetRequiredService<IPermissionCollection>();
 
@@ -16,9 +17,11 @@ public static class BootstrapperExtension
         var passwordHasher = scope.ServiceProvider.GetRequiredService<IPasswordHasher>();
         var settings = scope.ServiceProvider.GetRequiredService<ISettings>();
 
-        var realmCredentials = await credentialsGenerator.GenerateAsync("master", cancellation: default);
+        var clientCredentials = await credentialsGenerator.GenerateAsync("root", cancellation: default);
 
-        var defaultRealm = new Realm { Name = "master", ClientId = realmCredentials.ClientId };
+        var defaultRealm = new Realm { Name = "master" };
+        var defaultClient = new Client { Name = "root", Flows = [Grant.ClientCredentials] };
+
         var realmFilters = RealmFilters.WithSpecifications()
             .WithName("master")
             .Build();
@@ -31,7 +34,6 @@ public static class BootstrapperExtension
             return;
         }
 
-        defaultRealm.SecretHash = await passwordHasher.HashPasswordAsync(realmCredentials.ClientId + defaultRealm.Name);
         defaultRealm.Permissions = [.. RealmPermissions.SystemPermissions.Select(permissionName => new Permission
         {
             Id = Identifier.Generate<Permission>(),
@@ -39,10 +41,14 @@ public static class BootstrapperExtension
             RealmId = defaultRealm.Id
         })];
 
+        defaultClient.Secret = await passwordHasher.HashPasswordAsync(clientCredentials.ClientId + defaultRealm.Name);
+        defaultClient.Permissions = [.. defaultRealm.Permissions];
+
         realmProvider.SetRealm(defaultRealm);
 
         await realmCollection.InsertAsync(defaultRealm);
         await permissionCollection.InsertManyAsync(defaultRealm.Permissions);
+        await clientCollection.InsertAsync(defaultClient);
 
         var userFilters = UserFilters.WithSpecifications()
             .WithUsername(settings.Administration.Username)
