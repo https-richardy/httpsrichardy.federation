0e177334132f0a2d24bed7804750e53774436138
Richard Garcia <code.richardy@gmail.com>
2026-04-08 08:33:07 -0300

    feature(#20): this commit updates  the method to use client instead of realm

--- Applications/Backend/Source/HttpsRichardy.Federation.Application/Policies/RedirectUriPolicy.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Policies/RedirectUriPolicy.cs
@@ -3,12 +3,12 @@
 public sealed class RedirectUriPolicy : IRedirectUriPolicy
 {
     public Task<Result> EnsureRedirectUriIsAllowedAsync(
-        Realm realm, RedirectUri redirectUri, CancellationToken cancellation = default)
+        Client client, RedirectUri redirectUri, CancellationToken cancellation = default)
     {
         // according to oauth 2.0 spec (RFC 6749, section 3.1.2.3):
         // https://datatracker.ietf.org/doc/html/rfc6749#section-3.1.2.3
 
-        var isAllowed = realm.RedirectUris.Contains(redirectUri);
+        var isAllowed = client.RedirectUris.Contains(redirectUri);
 
         return isAllowed ?
             Task.FromResult(Result.Success()) :
