d52224689d1772f658f29d8c66dc52924ceb683c
Richard Garcia <code.richardy@gmail.com>
2026-04-09 20:56:23 -0300

    feature(#20): this commit introduces a new mapper dedicated to the clients module.

---
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Mappers/ClientMapper.cs
@@ -0,0 +1,14 @@
+﻿namespace HttpsRichardy.Federation.Application.Mappers;
+
+public static class ClientMapper
+{
+    public static Client AsClient(this ClientCreationScheme client, ClientCredentials credentials, Realm realm) => new()
+    {
+        Name = client.Name,
+        RealmId = realm.Id,
+        ClientId = credentials.ClientId,
+        Secret = credentials.ClientSecret,
+        Flows = [.. client.Flows],
+        RedirectUris = [.. client.RedirectUris.Select(uri => new RedirectUri(uri))]
+    };
+}
