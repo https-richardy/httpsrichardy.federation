d8710b8f06206b6270fbe473f06bb9b48dbc5289
Richard Garcia <code.richardy@gmail.com>
2026-04-09 21:18:19 -0300

    feature(#20): this commit introduces a new controller responsible for managing clients.

---
+++ Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Controllers/ClientsController.cs
@@ -0,0 +1,22 @@
+﻿namespace HttpsRichardy.Federation.WebApi.Controllers;
+
+[ApiController]
+[RealmRequired]
+[Route("api/v1/clients")]
+public sealed class ClientsController(IDispatcher dispatcher) : ControllerBase
+{
+    [HttpPost]
+    [Stability(Stability.Stable)]
+    [Authorize(Roles = Permissions.CreateClient)]
+    public async Task<IActionResult> CreateClientAsync([FromBody] ClientCreationScheme request, CancellationToken cancellation)
+    {
+        var result = await dispatcher.DispatchAsync(request, cancellation);
+
+        // we know the switch here is not strictly necessary since we only handle the success case,
+        // but we keep it for consistency with the rest of the codebase and to follow established patterns.
+        return result switch
+        {
+            { IsSuccess: true } => StatusCode(StatusCodes.Status201Created)
+        };
+    }
+}
