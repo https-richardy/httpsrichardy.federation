518420894184a7b3c32eb4e09d47baf6129f2ccb
Richard Garcia <code.richardy@gmail.com>
2026-04-14 20:55:14 -0300

    feature(#20): this commit introduces a new endpoint for obtaining clients, enabling filtering.

--- Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Controllers/ClientsController.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Controllers/ClientsController.cs
@@ -5,6 +5,23 @@
 [Route("api/v1/clients")]
 public sealed class ClientsController(IDispatcher dispatcher) : ControllerBase
 {
+    [HttpGet]
+    [Stability(Stability.Stable)]
+    [Authorize(Roles = Permissions.ViewClients)]
+    public async Task<IActionResult> GetClientsAsync([FromQuery] ClientsFetchParameters request, CancellationToken cancellation)
+    {
+        var result = await dispatcher.DispatchAsync(request, cancellation);
+
+        // we know the switch here is not strictly necessary since we only handle the success case,
+        // but we keep it for consistency with the rest of the codebase and to follow established patterns.
+
+        return result switch
+        {
+            { IsSuccess: true } when result.Data is not null =>
+                StatusCode(StatusCodes.Status200OK, result.Data)
+        };
+    }
+
     [HttpPost]
     [Stability(Stability.Stable)]
     [Authorize(Roles = Permissions.CreateClient)]
@@ -14,6 +31,7 @@ public sealed class ClientsController(IDispatcher dispatcher) : ControllerBase
 
         // we know the switch here is not strictly necessary since we only handle the success case,
         // but we keep it for consistency with the rest of the codebase and to follow established patterns.
+
         return result switch
         {
             { IsSuccess: true } => StatusCode(StatusCodes.Status201Created)
