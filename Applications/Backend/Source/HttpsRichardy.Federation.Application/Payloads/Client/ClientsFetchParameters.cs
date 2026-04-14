88691fdc1587129ed00860bcb7a992952987e089
Richard Garcia <code.richardy@gmail.com>
2026-04-14 20:31:54 -0300

    feature(#20): this commit introduces the record class representing the client filtering parameters.

---
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Payloads/Client/ClientsFetchParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace HttpsRichardy.Federation.Application.Payloads.Client;
+
+public sealed record ClientsFetchParameters :
+    IDispatchable<Result<Pagination<ClientScheme>>>
+{
+    public string? Name { get; set; }
+    public string? ClientId { get; set; }
+
+    public PaginationFilters? Pagination { get; set; }
+    public SortFilters? Sort { get; set; }
+
+    public DateOnly? CreatedAfter { get; set; }
+    public DateOnly? CreatedBefore { get; set; }
+}
