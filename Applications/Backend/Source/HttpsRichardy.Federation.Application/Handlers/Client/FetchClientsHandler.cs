e628a6177d4bb1e2a97f79efe72b10d9438ae66d
Richard Garcia <code.richardy@gmail.com>
2026-04-14 20:41:43 -0300

    feature(#20): this commit introduces a new handler for filtering and searching clients in the application.

---
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Handlers/Client/FetchClientsHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace HttpsRichardy.Federation.Application.Handlers.Client;
+
+public sealed class FetchClientsHandler(IClientCollection clientCollection) :
+    IDispatchHandler<ClientsFetchParameters, Result<Pagination<ClientScheme>>>
+{
+    public async Task<Result<Pagination<ClientScheme>>> HandleAsync(
+        ClientsFetchParameters parameters, CancellationToken cancellation = default)
+    {
+        var filters = ClientFilters.WithSpecifications()
+            .WithName(parameters.Name)
+            .WithClientId(parameters.ClientId)
+            .WithSort(parameters.Sort)
+            .WithPagination(parameters.Pagination)
+            .Build();
+
+        var clients = await clientCollection.GetClientsAsync(filters, cancellation);
+        var totalClients = await clientCollection.CountClientsAsync(filters, cancellation);
+
+        var pagination = new Pagination<ClientScheme>
+        {
+            Items = [.. clients.Select(client => ClientMapper.AsResponse(client))],
+            Total = (int)totalClients,
+            PageNumber = parameters.Pagination?.PageNumber ?? 1,
+            PageSize = parameters.Pagination?.PageSize ?? 20,
+        };
+
+        return Result<Pagination<ClientScheme>>.Success(pagination);
+    }
+}
