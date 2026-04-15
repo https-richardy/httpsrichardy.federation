e236dc4c110571291c27309a5b670a3099052aa8
Richard Garcia <code.richardy@gmail.com>
2026-04-14 21:06:54 -0300

    feature(#20): this commit includes the static class `clients conventions` with methods that define naming conventions and response types for client endpoints, using convention attributes.

---
+++ Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Conventions/ClientsConventions.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable IDE0060
+
+namespace HttpsRichardy.Federation.WebApi.Conventions;
+
+public static class ClientsConventions
+{
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    [ProducesResponseType(typeof(Pagination<ClientScheme>), StatusCodes.Status200OK)]
+    public static void GetClientsAsync(ClientsFetchParameters request, CancellationToken cancellation) { }
+
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public static void CreateClientAsync(ClientCreationScheme request, CancellationToken cancellation) { }
+}
