3ee4be3b94186624722340d16d708c1ff85b68ed
Richard Garcia <code.richardy@gmail.com>
2026-04-16 14:28:26 -0300

    feature(#20): this commit introduces error for client already having a specified permission

--- Applications/Backend/Source/HttpsRichardy.Federation.Domain/Errors/ClientErrors.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Domain/Errors/ClientErrors.cs
@@ -2,6 +2,11 @@ namespace HttpsRichardy.Federation.Domain.Errors;
 
 public static class ClientErrors
 {
+    public static readonly Error ClientAlreadyHasPermission = new(
+        Code: "#ERROR-8D71B",
+        Description: "The client already has the specified permission assigned."
+    );
+
     public static readonly Error ClientDoesNotExist = new(
         Code: "#ERROR-2D943",
         Description: "The client with the specified ID does not exist."
