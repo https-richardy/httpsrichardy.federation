edbf843650871bb7ffa20b586251cfb89a971a09
Richard Garcia <code.richardy@gmail.com>
2026-04-16 15:29:53 -0300

    feature(#20): this commit introduces client existence check during creation to prevent duplicates

--- Applications/Backend/Source/HttpsRichardy.Federation.Application/Handlers/Client/ClientCreationHandler.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Handlers/Client/ClientCreationHandler.cs
@@ -5,6 +5,18 @@ public sealed class ClientCreationHandler(IClientCredentialsGenerator credential
 {
     public async Task<Result> HandleAsync(ClientCreationScheme parameters, CancellationToken cancellation = default)
     {
+        var filters = ClientFilters.WithSpecifications()
+            .WithName(parameters.Name)
+            .Build();
+
+        var clients = await clientCollection.GetClientsAsync(filters, cancellation: cancellation);
+        var existingClient = clients.FirstOrDefault();
+
+        if (existingClient is not null)
+        {
+            return Result.Failure(ClientErrors.ClientAlreadyExists);
+        }
+
         var realm = realmProvider.GetCurrentRealm();
         var credentials = await credentialsGenerator.GenerateAsync(parameters.Name, cancellation);
 
