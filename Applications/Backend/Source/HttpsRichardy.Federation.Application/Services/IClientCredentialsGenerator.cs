7c2b5d00e7defa69f534e1d302fa5bbd9e22a93f
Richard Garcia <code.richardy@gmail.com>
2026-04-16 15:24:29 -0300

    feature(#20): update client credentials generation to use clientName instead of realmName

--- Applications/Backend/Source/HttpsRichardy.Federation.Application/Services/IClientCredentialsGenerator.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Services/IClientCredentialsGenerator.cs
@@ -2,5 +2,5 @@ namespace HttpsRichardy.Federation.Application.Services;
 
 public interface IClientCredentialsGenerator
 {
-    public Task<ClientCredentials> GenerateAsync(string realmName, CancellationToken cancellation = default);
+    public Task<ClientCredentials> GenerateAsync(string clientName, CancellationToken cancellation = default);
 }
--- Applications/Backend/Source/HttpsRichardy.Federation.Infrastructure/Security/ClientCredentialsGenerator.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Infrastructure/Security/ClientCredentialsGenerator.cs
@@ -2,14 +2,14 @@ namespace HttpsRichardy.Federation.Infrastructure.Security;
 
 public sealed class ClientCredentialsGenerator(IPasswordHasher passwordHasher) : IClientCredentialsGenerator
 {
-    public async Task<ClientCredentials> GenerateAsync(string realmName, CancellationToken cancellation = default)
+    public async Task<ClientCredentials> GenerateAsync(string clientName, CancellationToken cancellation = default)
     {
         var bytes = new byte[32];
 
         RandomNumberGenerator.Fill(bytes);
 
         var clientId = Convert.ToHexString(bytes).ToLowerInvariant();
-        var clientSecret = await passwordHasher.HashPasswordAsync(clientId + realmName);
+        var clientSecret = await passwordHasher.HashPasswordAsync(clientId + clientName);
 
         return new ClientCredentials
         {
