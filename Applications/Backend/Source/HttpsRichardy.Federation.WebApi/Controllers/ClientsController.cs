442f74649122673189464d738e7a478fe410c0cf
Richard Garcia <code.richardy@gmail.com>
2026-04-17 20:40:49 -0300

    chore(#20): this commit removes unnecessary and irrelevant comments in the current context

--- Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Controllers/ClientsController.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.WebApi/Controllers/ClientsController.cs
@@ -30,9 +30,6 @@ public sealed class ClientsController(IDispatcher dispatcher) : ControllerBase
     {
         var result = await dispatcher.DispatchAsync(request, cancellation);
 
-        // we know the switch here is not strictly necessary since we only handle the success case,
-        // but we keep it for consistency with the rest of the codebase and to follow established patterns.
-
         return result switch
         {
             { IsSuccess: true } when result.Data is not null =>
