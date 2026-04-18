2653bbc352c8916d456bce006389c61d4cf0839f
Richard Garcia <code.richardy@gmail.com>
2026-04-18 02:07:21 -0300

    refactor(#20): this commit removes client-id property from realm fetch parameters record

--- Packages/Federation.Sdk.Contracts/Source/Payloads/Realm/RealmFetchParameters.cs
+++ Packages/Federation.Sdk.Contracts/Source/Payloads/Realm/RealmFetchParameters.cs
@@ -3,9 +3,7 @@ namespace HttpsRichardy.Federation.Sdk.Contracts.Payloads.Realm;
 public sealed record RealmFetchParameters
 {
     public string? Id { get; init; }
-
     public string? Name { get; init; }
-    public string? ClientId { get; init; }
     public bool? IncludeDeleted { get; init; }
 
     public int PageNumber { get; init; } = 1;
