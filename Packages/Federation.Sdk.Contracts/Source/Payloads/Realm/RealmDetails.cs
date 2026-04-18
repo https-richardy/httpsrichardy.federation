d679784cad48084be1963cf9f9c385fb5612e315
Richard Garcia <code.richardy@gmail.com>
2026-04-18 02:06:16 -0300

    refactor(#20): this commit removes client-id and client-secret from realm details record

--- Packages/Federation.Sdk.Contracts/Source/Payloads/Realm/RealmDetails.cs
+++ Packages/Federation.Sdk.Contracts/Source/Payloads/Realm/RealmDetails.cs
@@ -5,6 +5,4 @@ public sealed record RealmDetails
     public string Id { get; init; } = default!;
     public string Name { get; init; } = default!;
     public string? Description { get; init; } = default!;
-    public string ClientId { get; init; } = default!;
-    public string ClientSecret { get; init; } = default!;
 }
