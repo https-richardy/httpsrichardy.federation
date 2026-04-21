57135a8e2a99835eaeaaee45bad83e856226040b
Richard Garcia <code.richardy@gmail.com>
2026-04-21 15:05:56 -0300

    feature(#22): this commit updates handler to include realm collection and enhance secret retrieval logic

--- Applications/Backend/Source/HttpsRichardy.Federation.Application/Handlers/Connect/FetchJsonWebKeysHandler.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Handlers/Connect/FetchJsonWebKeysHandler.cs
@@ -1,13 +1,30 @@
 namespace HttpsRichardy.Federation.Application.Handlers.Connect;
 
-public sealed class FetchJsonWebKeysHandler(ISecretCollection collection) :
+public sealed class FetchJsonWebKeysHandler(ISecretCollection collection, IRealmCollection realmCollection) :
     IDispatchHandler<FetchJsonWebKeysParameters, Result<JsonWebKeySetScheme>>
 {
     public async Task<Result<JsonWebKeySetScheme>> HandleAsync(
         FetchJsonWebKeysParameters parameters, CancellationToken cancellation = default)
     {
-        var secret = await collection.GetSecretAsync(cancellation: cancellation);
-        var jwks = JsonWebKeysMapper.AsJsonWebKeySetScheme(secret);
+        var realmFilters = RealmFilters.WithSpecifications()
+            .WithName(parameters.Realm)
+            .Build();
+
+        var realms = await realmCollection.GetRealmsAsync(realmFilters, cancellation);
+        var realm = realms.FirstOrDefault();
+
+        if (realm is null)
+        {
+            return Result<JsonWebKeySetScheme>.Failure(RealmErrors.RealmDoesNotExist);
+        }
+
+        var filters = SecretFilters.WithSpecifications()
+            .WithCanValidate()
+            .WithRealm(realm.Id)
+            .Build();
+
+        var secrets = await collection.GetSecretsAsync(filters, cancellation);
+        var jwks = JsonWebKeysMapper.AsJsonWebKeySetScheme(secrets);
 
         return Result<JsonWebKeySetScheme>.Success(jwks);
     }
