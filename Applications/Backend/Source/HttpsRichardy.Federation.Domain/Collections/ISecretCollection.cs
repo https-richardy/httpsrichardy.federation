eb7c9ac8b4f496b6303f76d80865434f820e3826
Richard Garcia <code.richardy@gmail.com>
2026-04-21 11:44:41 -0300

    refactor(#22): this commit refactors the secrets collection with filters and counts, updates the constructor to accept a realm provider, and implements aggregation pipelines in MongoDB for searching and counting secrets.

--- Applications/Backend/Source/HttpsRichardy.Federation.Domain/Collections/ISecretCollection.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Domain/Collections/ISecretCollection.cs
@@ -2,5 +2,13 @@ namespace HttpsRichardy.Federation.Domain.Collections;
 
 public interface ISecretCollection : IAggregateCollection<Secret>
 {
-    public Task<Secret> GetSecretAsync(CancellationToken cancellation = default);
+    public Task<IReadOnlyCollection<Secret>> GetSecretsAsync(
+        SecretFilters filters,
+        CancellationToken cancellation = default
+    );
+
+    public Task<System.Numerics.BigInteger> CountSecretsAsync(
+        SecretFilters filters,
+        CancellationToken cancellation = default
+    );
 }
