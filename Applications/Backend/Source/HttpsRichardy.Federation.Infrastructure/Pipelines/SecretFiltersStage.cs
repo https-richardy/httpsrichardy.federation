215ae83f0a474c94c5642777980ff3cf41fdd5be
Richard Garcia <code.richardy@gmail.com>
2026-04-21 11:38:46 -0300

    feature(#22): this commit introduces secret filters stage for dynamic secret filtering, allowing dynamic filters to be applied in MongoDB pipelines for the secret entity

---
+++ Applications/Backend/Source/HttpsRichardy.Federation.Infrastructure/Pipelines/SecretFiltersStage.cs
@@ -0,0 +1,40 @@
+﻿namespace HttpsRichardy.Federation.Infrastructure.Pipelines;
+
+public static class SecretFiltersStage
+{
+    public static PipelineDefinition<Secret, BsonDocument> FilterSecrets(this PipelineDefinition<Secret, BsonDocument> pipeline,
+        SecretFilters filters, IRealmProvider realmProvider)
+    {
+        var realm = realmProvider.GetCurrentRealm();
+        var now = filters.Now ?? DateTime.UtcNow;
+
+        var definitions = new List<FilterDefinition<BsonDocument>>
+        {
+            FilterDefinitions.MatchIfNotEmpty(Documents.Secret.Id, filters.Id),
+            FilterDefinitions.MatchIfNotEmpty(Documents.Secret.RealmId, filters.RealmId ?? realm?.Id),
+        };
+
+        if (filters.CanSign is true)
+        {
+            definitions.Add(Builders<BsonDocument>.Filter.Or(
+                    Builders<BsonDocument>.Filter.Eq(Documents.Secret.ExpiresAt, BsonNull.Value),
+                    Builders<BsonDocument>.Filter.Gt(Documents.Secret.ExpiresAt, now)
+            ));
+        }
+
+        // a secret is in the grace period if it has already expired for signing and its grace period has not ended yet.
+
+        if (filters.InGracePeriod is true)
+        {
+            definitions.Add(Builders<BsonDocument>.Filter.And(
+                    Builders<BsonDocument>.Filter.Ne(Documents.Secret.ExpiresAt, BsonNull.Value),
+                    Builders<BsonDocument>.Filter.Lte(Documents.Secret.ExpiresAt, now),
+
+                    Builders<BsonDocument>.Filter.Ne(Documents.Secret.GracePeriodEndsAt, BsonNull.Value),
+                    Builders<BsonDocument>.Filter.Gt(Documents.Secret.GracePeriodEndsAt, now)
+                ));
+        }
+
+        return pipeline.Match(Builders<BsonDocument>.Filter.And(definitions));
+    }
+}
