640e42ff4400e8731fd3c6cac365dd7fbdfc43a0
Richard Garcia <code.richardy@gmail.com>
2026-04-21 15:39:19 -0300

    feature(#22): this commit updates method to include realm for JWKS URI generation

--- Applications/Backend/Source/HttpsRichardy.Federation.Application/Mappers/OpenIDMapper.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Mappers/OpenIDMapper.cs
@@ -2,13 +2,13 @@ namespace HttpsRichardy.Federation.Application.Mappers;
 
 public static class ConnectMapper
 {
-    public static OpenIDConfigurationScheme AsConfiguration(Uri baseUri)
+    public static OpenIDConfigurationScheme AsConfiguration(Uri baseUri, string realm)
     {
         var issuer = baseUri.GetLeftPart(UriPartial.Authority);
         var authorizeUri = new Uri(baseUri, OpenIDEndpoints.Authorize);
         var tokenUri = new Uri(baseUri, OpenIDEndpoints.Token);
         var userInfoUri = new Uri(baseUri, OpenIDEndpoints.UserInfo);
-        var jwksUri = new Uri(baseUri, OpenIDEndpoints.Jwks);
+        var jwksUri = new Uri(baseUri, $"{realm}/{OpenIDEndpoints.Jwks}");
 
         var configuration = new OpenIDConfigurationScheme
         {
