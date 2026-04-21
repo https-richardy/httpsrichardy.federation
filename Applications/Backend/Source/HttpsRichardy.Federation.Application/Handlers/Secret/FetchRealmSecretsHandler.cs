d5afab9f4719af51831d6b136ba70d302d86ec4d
Richard Garcia <code.richardy@gmail.com>
2026-04-21 20:48:42 -0300

    feature(#22): this commit introduces validation to ensure that the realm exists before fetching secrets.

--- Applications/Backend/Source/HttpsRichardy.Federation.Application/Handlers/Secret/FetchRealmSecretsHandler.cs
+++ Applications/Backend/Source/HttpsRichardy.Federation.Application/Handlers/Secret/FetchRealmSecretsHandler.cs
@@ -1,11 +1,23 @@
 namespace HttpsRichardy.Federation.Application.Handlers.Secret;
 
-public sealed class FetchRealmSecretsHandler(ISecretCollection collection) :
+public sealed class FetchRealmSecretsHandler(ISecretCollection collection, IRealmCollection realmCollection) :
     IDispatchHandler<FetchRealmSecretsParameters, Result<IReadOnlyCollection<SecretScheme>>>
 {
     public async Task<Result<IReadOnlyCollection<SecretScheme>>> HandleAsync(
         FetchRealmSecretsParameters parameters, CancellationToken cancellation = default)
     {
+        var realmFilters = RealmFilters.WithSpecifications()
+            .WithIdentifier(parameters.RealmId)
+            .Build();
+
+        var realms = await realmCollection.GetRealmsAsync(realmFilters, cancellation);
+        var realm = realms.FirstOrDefault();
+
+        if (realm is null)
+        {
+            return Result<IReadOnlyCollection<SecretScheme>>.Failure(RealmErrors.RealmDoesNotExist);
+        }
+
         var filters = SecretFilters.WithSpecifications()
             .WithRealm(parameters.RealmId)
             .Build();
