1a78efc5071884ab89b8f4fb8d279822a09c022d
Richard Garcia <code.richardy@gmail.com>
2026-04-21 11:21:29 -0300

    feature(#22): this commit includes filtering classes to simplify the creation and configuration of filters related to secrets. This improves the flexibility and reusability of filtering criteria within the domain.

---
+++ Applications/Backend/Source/HttpsRichardy.Federation.Domain/Filtering/Builders/SecretFiltersBuilder.cs
@@ -0,0 +1,37 @@
+﻿namespace HttpsRichardy.Federation.Domain.Filtering.Builders;
+
+public sealed class SecretFiltersBuilder :
+    FiltersBuilderBase<SecretFilters, SecretFiltersBuilder>
+{
+    public SecretFiltersBuilder ForRealm(string? realmId)
+    {
+        if (!string.IsNullOrWhiteSpace(realmId))
+            _filters.RealmId = realmId;
+
+        return this;
+    }
+
+    public SecretFiltersBuilder WithCanSign(DateTime? now = null)
+    {
+        _filters.CanSign = true;
+        _filters.Now = now ?? DateTime.UtcNow;
+
+        return this;
+    }
+
+    public SecretFiltersBuilder WithInGrace(DateTime? now = null)
+    {
+        _filters.InGracePeriod = true;
+        _filters.Now = now ?? DateTime.UtcNow;
+
+        return this;
+    }
+
+    public SecretFiltersBuilder WithExpired(DateTime? now = null)
+    {
+        _filters.IsExpired = true;
+        _filters.Now = now ?? DateTime.UtcNow;
+
+        return this;
+    }
+}
---
+++ Applications/Backend/Source/HttpsRichardy.Federation.Domain/Filtering/SecretFilters.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpsRichardy.Federation.Domain.Filtering;
+
+public sealed class SecretFilters : Filters
+{
+    public string? RealmId { get; set; }
+    public bool? CanSign { get; set; }
+    public bool? InGracePeriod { get; set; }
+    public bool? IsExpired { get; set; }
+    public DateTime? Now { get; set; }
+}
